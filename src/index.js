982b096227d6cfe6f05c522baf39524c68a06918
Oliver <113215526+x4qb@users.noreply.github.com>
2025-09-24 09:37:23 -0400

    added index.html to all current paths and added headers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import index from '../public/index.html';
+
 export default {
 	async fetch(request, env, ctx) {
         const url = new URL(request.url);
@@ -5,10 +7,19 @@ export default {
         const pathname = url.pathname;
         const method = request.method;
 
-		return new Response('hi', {
+		return new Response(index, {
+			status: 200,
 			headers: {
-				'Content-Type': 'text/html'
-			},
-		});
+				'Content-Security-Policy': `default-src 'self';`,
+				'Strict-Transport-Policy': 'max-age=63072000; preload',
+				'Cross-Origin-Resource-Policy': 'same-origin',
+				'Cross-Origin-Embedder-Policy': 'require-corp',
+				'Cross-Origin-Opener-Policy': 'same-origin',
+				'Referrer-Policy': 'no-referrer-when-downgrade',
+				'Cache-Control': 'public, max-age=31536000',
+				'ETag': 'some-hash',
+				'Theme-Color': '#c6c5c4'
+			}
+		})
 	},
 };
