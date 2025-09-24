fd51a3ebad97c3e8a586ec3e38e3383ecceb18fc
Oliver <113215526+x4qb@users.noreply.github.com>
2025-09-24 09:45:08 -0400

    fixed some content security policy headers for the styling and also added a nonce for future javascript additions

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,5 +1,5 @@
 export const commonHeaders = {
-	'Content-Security-Policy': `default-src 'self';`,
+	'Content-Security-Policy': `default-src 'self'; style-src 'self' 'unsafe-inline' 'nonce-mainjs'`,
 	'Strict-Transport-Policy': 'max-age=63072000; preload',
     'Cross-Origin-Resource-Policy': 'same-origin',
 	'Cross-Origin-Embedder-Policy': 'require-corp',
